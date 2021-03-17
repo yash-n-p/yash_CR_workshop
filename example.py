2ed9e5d12856aeda6573cc624b176dd775ee48bb
Yash Niranjan Poojary <yash@student.chalmers.se>
2021-03-17 10:33:04 +0100

    step 5

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ def add(a, b):
 
 
 def test_add():
-    assert add(2, 3) == 6
+    assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
 
 
@@ -12,5 +12,5 @@ def subtract(a, b):
 
 
 # uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+def test_subtract():
+    assert subtract(2, 3) == -1
